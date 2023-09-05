a0a834551a1e5533207ce2fccc453c5c680d1afd
kabirz <jxwazxzhp@126.com>
2023-09-05 14:49:40 +0800

    line-bar init
    Signed-off-by: kabirz <jxwazxzhp@126.com>

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,147 @@
-use ansi_term::{Style, Colour::Fixed};
+mod line;
+mod tab;
+
+use std::cmp::{max, min};
+use std::collections::BTreeMap;
+use std::convert::TryInto;
+
+use tab::get_tab_to_focus;
 use zellij_tile::prelude::*;
 
-use std::collections::{HashMap, BTreeMap};
+use crate::line::tab_line;
+use crate::tab::tab_style;
+
+#[derive(Debug, Default)]
+pub struct LinePart {
+    part: String,
+    len: usize,
+    tab_index: Option<usize>,
+}
 
 #[derive(Default)]
 struct State {
-    mode_log: HashMap<String, usize>,
-    tabs: Vec<String>,
-    test_runs: usize,
-    userspace_configuration: BTreeMap<String, String>,
+    tabs: Vec<TabInfo>,
+    active_tab_idx: usize,
+    mode_info: ModeInfo,
+    tab_line: Vec<LinePart>,
 }
+
+static ARROW_SEPARATOR: &str = "";
 
 register_plugin!(State);
 
 impl ZellijPlugin for State {
-    fn load(&mut self, configuration: BTreeMap<String, String>) {
-        self.userspace_configuration = configuration;
-        // we need the ReadApplicationState permission to receive the ModeUpdate and TabUpdate
-        // events
-        // we need the RunCommands permission to run "cargo test" in a floating window
-        request_permission(&[PermissionType::ReadApplicationState, PermissionType::RunCommands]);
-        subscribe(&[EventType::ModeUpdate, EventType::TabUpdate, EventType::Key]);
+    fn load(&mut self, _configuration: BTreeMap<String, String>) {
+        set_selectable(false);
+        subscribe(&[
+            EventType::TabUpdate,
+            EventType::ModeUpdate,
+            EventType::Mouse,
+        ]);
     }
 
     fn update(&mut self, event: Event) -> bool {
         let mut should_render = false;
         match event {
             Event::ModeUpdate(mode_info) => {
-                let mode = format!("{:?}", mode_info.mode);
-                let count = self.mode_log.entry(mode).or_insert(0);
-                *count += 1;
-                should_render = true;
-            }
-            Event::TabUpdate(tab_info) => {
-                self.tabs = tab_info.iter().map(|t| t.name.clone()).collect();
-                should_render = true;
-            }
-            Event::Key(key) => {
-                if let Key::Char('n') = key {
-                    self.test_runs += 1;
-                    open_command_pane_floating(CommandToRun {
-                        path: "cargo".into(),
-                        args: vec!["test".to_owned()],
-                        cwd: None,
-                    });
+                if self.mode_info != mode_info {
+                    should_render = true;
                 }
-            }
-            _ => (),
+                self.mode_info = mode_info
+            },
+            Event::TabUpdate(tabs) => {
+                if let Some(active_tab_index) = tabs.iter().position(|t| t.active) {
+                    // tabs are indexed starting from 1 so we need to add 1
+                    let active_tab_idx = active_tab_index + 1;
+                    if self.active_tab_idx != active_tab_idx || self.tabs != tabs {
+                        should_render = true;
+                    }
+                    self.active_tab_idx = active_tab_idx;
+                    self.tabs = tabs;
+                } else {
+                    eprintln!("Could not find active tab.");
+                }
+            },
+            Event::Mouse(me) => match me {
+                Mouse::LeftClick(_, col) => {
+                    let tab_to_focus = get_tab_to_focus(&self.tab_line, self.active_tab_idx, col);
+                    if let Some(idx) = tab_to_focus {
+                        switch_tab_to(idx.try_into().unwrap());
+                    }
+                },
+                Mouse::ScrollUp(_) => {
+                    switch_tab_to(min(self.active_tab_idx + 1, self.tabs.len()) as u32);
+                },
+                Mouse::ScrollDown(_) => {
+                    switch_tab_to(max(self.active_tab_idx.saturating_sub(1), 1) as u32);
+                },
+                _ => {},
+            },
+            _ => {
+                eprintln!("Got unrecognized event: {:?}", event);
+            },
         };
         should_render
     }
 
-    fn render(&mut self, rows: usize, cols: usize) {
-        let colored_rows = color_bold(CYAN, &rows.to_string());
-        let colored_cols = color_bold(CYAN, &cols.to_string());
-        println!("");
-        println!("I have {} rows and {} columns", colored_rows, colored_cols);
-        println!("");
-        println!("{} {:#?}", color_bold(GREEN, "I was started with the following user configuration:"), self.userspace_configuration);
-        println!("");
-        println!("{}", color_bold(GREEN, "Modes:"));
-        for (mode, count) in &self.mode_log {
-            let count = color_bold(ORANGE, &count.to_string());
-            println!("{} -> Changed {} times", mode, count);
+    fn render(&mut self, _rows: usize, cols: usize) {
+        if self.tabs.is_empty() {
+            return;
         }
-        println!("");
-        let current_tabs = color_bold(GREEN, "Current Tabs:");
-        let comma = color_bold(ORANGE, ", ");
-        println!("{} {}", current_tabs, self.tabs.join(&comma));
-        println!("");
-        if self.test_runs > 0 {
-            let test_run_count = color_bold(CYAN, &self.test_runs.to_string());
-            println!("Ran tests {} times!", test_run_count);
+        let mut all_tabs: Vec<LinePart> = vec![];
+        let mut active_tab_index = 0;
+        let mut active_swap_layout_name = None;
+        let mut is_swap_layout_dirty = false;
+        let mut is_alternate_tab = false;
+        for t in &mut self.tabs {
+            let mut tabname = t.name.clone();
+            if t.active && self.mode_info.mode == InputMode::RenameTab {
+                if tabname.is_empty() {
+                    tabname = String::from("Enter name...");
+                }
+                active_tab_index = t.position;
+            } else if t.active {
+                active_tab_index = t.position;
+                is_swap_layout_dirty = t.is_swap_layout_dirty;
+                active_swap_layout_name = t.active_swap_layout_name.clone();
+            }
+            let tab = tab_style(
+                tabname,
+                t,
+                is_alternate_tab,
+                self.mode_info.style.colors,
+                self.mode_info.capabilities,
+            );
+            is_alternate_tab = !is_alternate_tab;
+            all_tabs.push(tab);
+        }
+        self.tab_line = tab_line(
+            self.mode_info.session_name.as_deref(),
+            all_tabs,
+            active_tab_index,
+            cols.saturating_sub(1),
+            self.mode_info.style.colors,
+            self.mode_info.capabilities,
+            self.mode_info.style.hide_session_name,
+            self.mode_info.mode,
+            &active_swap_layout_name,
+            is_swap_layout_dirty,
+        );
+        let output = self
+            .tab_line
+            .iter()
+            .fold(String::new(), |output, part| output + &part.part);
+        let background = match self.mode_info.style.colors.theme_hue {
+            ThemeHue::Dark => self.mode_info.style.colors.black,
+            ThemeHue::Light => self.mode_info.style.colors.white,
+        };
+        match background {
+            PaletteColor::Rgb((r, g, b)) => {
+                print!("{}\u{1b}[48;2;{};{};{}m\u{1b}[0K", output, r, g, b);
+            },
+            PaletteColor::EightBit(color) => {
+                print!("{}\u{1b}[48;5;{}m\u{1b}[0K", output, color);
+            },
         }
     }
-}
-
-pub const CYAN: u8 = 51;
-pub const GRAY_LIGHT: u8 = 238;
-pub const GRAY_DARK: u8 = 245;
-pub const WHITE: u8 = 15;
-pub const BLACK: u8 = 16;
-pub const RED: u8 = 124;
-pub const GREEN: u8 = 154;
-pub const ORANGE: u8 = 166;
-
-fn color_bold(color: u8, text: &str) -> String {
-    format!("{}", Style::new().fg(Fixed(color)).bold().paint(text))
 }
